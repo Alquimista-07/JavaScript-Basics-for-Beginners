56c2c6ccad52894edf7c5e8132def129fbc7306c
Alquimista-07 <jdca.07@gmail.com>
2023-02-17 11:33:29 -0500

    8. 1- Variables

--- 2. Sección 2 Basics/js-basics/index.js
+++ 2. Sección 2 Basics/js-basics/index.js
@@ -1,2 +1,16 @@
-// This is my first JavaScript code
-console.log('Hello World');
+let name = 'Ariadna';
+
+console.log(name);
+
+// Rules
+//
+// Cannot be a reseved keyword: le, if, for, etc
+// Shoul be meaningful: a, b, x, etc.
+// Cannot start with a number: 1name
+// Cannot contain a space o hyphen (-)
+// Are case-sensitive
+
+let firstName = 'Mosh', lastName = 'Hamedani';
+
+let firstName2 = 'Mosh';
+let lastName2 = 'Hmedani'; 
