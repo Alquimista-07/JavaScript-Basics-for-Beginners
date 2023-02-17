a27a1c2c92ace46f6027306b07784630993d1b5f
Alquimista-07 <jdca.07@gmail.com>
2023-02-17 11:57:16 -0500

    9. 2- Constants

--- 2. Sección 2 Basics/js-basics/index.js
+++ 2. Sección 2 Basics/js-basics/index.js
@@ -14,3 +14,13 @@ let firstName = 'Mosh', lastName = 'Hamedani';
 
 let firstName2 = 'Mosh';
 let lastName2 = 'Hmedani'; 
+
+// Variable
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+// Constant
+const interestRate2 = 0.3;
+interestRate2 = 1;
+console.log(interestRate2); // Error: you can't change a constant
