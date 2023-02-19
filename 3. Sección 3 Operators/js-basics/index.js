067d9a9611c51aceea2530658f2c44de5f7e8407
Alquimista-07 <jdca.07@gmail.com>
2023-02-19 10:11:07 -0500

    17. Arithmetic Operators

---
+++ 3. Sección 3 Operators/js-basics/index.js
@@ -0,0 +1,35 @@
+// Arithmetic Operators
+
+let x = 10;
+let x2 = 10;
+let y = 3;
+let y2 = 3;
+
+console.log('-----------------------------');
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / 7);
+console.log(x % 7);
+console.log(x ** y);
+console.log('-----------------------------');
+
+// Increment operators (++)
+console.log('*****************************');
+console.log(++x); // -> 11
+console.log('*****************************');
+
+console.log('*****************************');
+console.log(x2++);
+console.log(x2); //-> 11
+console.log('*****************************');
+
+// Decrement operators (--)
+console.log('*****************************');
+console.log(--y);
+console.log('*****************************');
+
+console.log('*****************************');
+console.log(y2--);
+console.log(y2);
+console.log('*****************************');
