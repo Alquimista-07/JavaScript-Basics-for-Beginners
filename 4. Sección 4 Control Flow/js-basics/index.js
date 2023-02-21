efa9d8ced1355a3a52bace587d84a6862b098f9e
Alquimista-07 <jdca.07@gmail.com>
2023-02-21 11:42:03 -0500

    31. 4- While

--- 4. Sección 4 Control Flow/js-basics/index.js
+++ 4. Sección 4 Control Flow/js-basics/index.js
@@ -46,3 +46,13 @@ for (let i = 1; i <= 5; i++) {
 for (let i = 5; i >= 1; i--) {
     if ( i % 2 !== 0 ) console.log('Hello World', i);
 }
+
+// While
+console.log('**********************************');
+
+let i = 0;
+
+while (i <= 5) {
+    if ( i % 2 !== 0 ) console.log( i );
+    i++;
+}
