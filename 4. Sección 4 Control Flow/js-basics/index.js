7459650432f086ceeeebe934b25a945d05aaf552
Alquimista-07 <jdca.07@gmail.com>
2023-02-22 14:39:33 -0500

    42. 15- Exercise- Count Truthy

--- 4. Sección 4 Control Flow/js-basics/index.js
+++ 4. Sección 4 Control Flow/js-basics/index.js
@@ -222,3 +222,30 @@ function showNumbers(limit){
         console.log(i, message);
     }
 }
+
+// 42. 15- Exercise- Count Truthy
+console.log('***********************************');
+
+// Falsy
+// undefined
+// null
+// ''
+// false
+// 0
+// NaN
+
+const array = [0, null, undefined, '', 2, 3];
+
+console.log('Count: ', countTruthy(array));
+
+function countTruthy(array){
+    
+    let count = 0;
+    for (let value of array) {
+        if (value){
+            count++;
+        }
+    return count;
+    }
+
+}
