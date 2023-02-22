bf5d38a857dc33848862fcb6825ed7c03091b413
Alquimista-07 <jdca.07@gmail.com>
2023-02-22 15:03:34 -0500

    46. 19- Exercise - Stars

--- 4. Sección 4 Control Flow/js-basics/index.js
+++ 4. Sección 4 Control Flow/js-basics/index.js
@@ -330,3 +330,20 @@ function calculateAverge(array){
     return sum / array.length;
 
 }
+
+// Exercise - Stars
+console.log('***************************');
+
+showStars(2);
+
+function showStars(rows){
+
+    for( let row = 1; row <= rows; row++){
+        let pattern = '';
+        for( let i = 0; i < row; i++){
+            pattern += '*';
+        }
+        console.log(pattern);
+    }
+
+}
