cef26255bbe6e2508500a67770a8ad3eb799d3bb
Alquimista-07 <jdca.07@gmail.com>
2023-02-22 15:00:00 -0500

    45. 18- Exercise - Grade

--- 4. Sección 4 Control Flow/js-basics/index.js
+++ 4. Sección 4 Control Flow/js-basics/index.js
@@ -246,7 +246,7 @@ function countTruthy(array){
             count++;
         }
     }
-    
+
     return count;
 
 }
@@ -292,5 +292,41 @@ function sumVals(limit){
     }
 
     return sumVal;
+
+}
+
+// Exercise - Grade
+console.log('********************************');
+
+const marks = [80, 80, 50];
+
+// Average = 70
+// 0-59: F
+// 60-69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
+
+console.log(calculateGrade(marks));
+
+function calculateGrade(marks){
+
+    const average = calculateAverge(marks);
+
+    if ( average < 60 ) return 'F';
+    if (average < 70) return 'D';
+    if (average < 80) return 'C';
+    if (average < 90) return 'B';
+    return 'A';
+
+}
+
+function calculateAverge(array){
+
+    let sum = 0;
+
+    for( let value of array )
+        sum += value;
+    return sum / array.length;
 
 }
