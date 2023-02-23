7fcc70cdd8bd111bfca8327402c272b5ac7cbf81
Alquimista-07 <jdca.07@gmail.com>
2023-02-23 11:24:31 -0500

    49. 2- Factory Functions

--- 5. Sección 5 Objects/js-basics/index.js
+++ 5. Sección 5 Objects/js-basics/index.js
@@ -1,5 +1,5 @@
 // Basics
-
+/*
 // Object-oriented Programming (OOP)
 const circle = {
 
@@ -16,3 +16,23 @@ const circle = {
 };
 
 circle.draw(); // Method
+*/
+
+// Factory Functions
+console.log('**********************************');
+
+function createCircle(radius){
+
+    return {
+        radius,
+        draw(){
+            console.log('draw');
+        }
+    };
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(2);
+console.log(circle2);
