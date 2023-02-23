55cbae89eb7361e2ff29716c8a9d39caf958925e
Alquimista-07 <jdca.07@gmail.com>
2023-02-23 11:16:03 -0500

    48. 1- Basics

---
+++ 5. Sección 5 Objects/js-basics/index.js
@@ -0,0 +1,18 @@
+// Basics
+
+// Object-oriented Programming (OOP)
+const circle = {
+
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function(){
+        console.log('draw');
+    }
+
+};
+
+circle.draw(); // Method
