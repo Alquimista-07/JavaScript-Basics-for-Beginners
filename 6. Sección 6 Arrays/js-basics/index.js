8106dd2868f40b958a37108c4c8d21d67edaf5ad
Alquimista-07 <jdca.07@gmail.com>
2023-02-27 12:18:13 -0500

    70. 3- Finding Elements (Primitives)

--- 6. Sección 6 Arrays/js-basics/index.js
+++ 6. Sección 6 Arrays/js-basics/index.js
@@ -1,14 +1,31 @@
 // Adding Elements
 const numbers = [3, 4];
 
-// End
+// ---End---
 numbers.push(5, 6);
 console.log(numbers);
 
-// Beginnining
+// ---Beginnining---
 numbers.unshift(1, 2);
 console.log(numbers);
 
-// Middle
+// ---Middle---
 numbers.splice(2, 0, 'a', 'b');
 console.log(numbers);
+
+// Finding Elements (Primitives)
+console.log('**********************************');
+
+const numbers2 = [1, 2, 3, 1, 4];
+
+console.log(numbers2.indexOf(1));
+console.log(numbers2.indexOf(1, 2));
+console.log(numbers2.indexOf('1'));
+
+console.log(numbers2.lastIndexOf(1));
+
+console.log(numbers2.indexOf(1) !== -1);
+console.log(numbers2.indexOf('1') !== -1);
+
+console.log(numbers2.includes(1));
+console.log(numbers2.includes('1'));
