f354762ebedaaca54bd3b81e1dd259b99089c766
Alquimista-07 <jdca.07@gmail.com>
2023-03-04 15:42:26 -0500

    94. 4- The Rest Operator

--- 7. Sección 7 Functions/js-basics/index.js
+++ 7. Sección 7 Functions/js-basics/index.js
@@ -60,3 +60,20 @@ console.log(sum(1, 2, 3, 4, 5));
 
 //----------------------------------------
 console.log(sum2(1, 2, 3, 4, 5, 10));
+
+// The Rest Operator
+console.log('********************************************');
+
+// We use the rest operator which in writing is equal to the operator 
+// spred (...) but we must not confuse it since the spred is used with arrays.
+function sum3(discount, ...prices) {
+    console.log(prices);
+    const total =  prices.reduce((a, b) => a + b);
+    return total * (1 - discount);
+}
+console.log(sum3(0.1, 20, 30));
+
+function sum4(args) {
+    console.log(args);
+}
+sum4(1,2,3,5,10);
