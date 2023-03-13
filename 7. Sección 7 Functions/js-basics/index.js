fafd3f31043fdb79caeee06a81cd8daa559c69f0
Alquimista-07 <jdca.07@gmail.com>
2023-03-13 09:59:02 -0500

    104. 14- Exercise 3- Error Handling

--- 7. Sección 7 Functions/js-basics/index.js
+++ 7. Sección 7 Functions/js-basics/index.js
@@ -312,3 +312,27 @@ const circleArea = {
 };
 
 console.log(circleArea.area);
+
+// Exercise 3- Error Handling
+console.log('*************************************');
+
+try {
+    const numbers = [1, 2, 3, 4];
+    const count = countOcurrences(null, 1);
+    console.log(count);
+}
+
+catch (e) {
+    console.log(e.message);
+}   
+
+function countOcurrences(array, searchElement){
+    if( !Array.isArray(array) ){
+        throw new Error('Invalid array');
+    }
+
+    return array.reduce((accumulator, current) => {
+        const ocurrence = (current === searchElement) ? 1 : 0;
+        return accumulator + ocurrence
+    })
+}
